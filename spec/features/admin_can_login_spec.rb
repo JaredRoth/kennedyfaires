2f0995d6d75f07638bb4558964355d7d372c9f3a
Jared Roth <jared.roth@gmail.com>
2016-11-14 13:49:47 -0800

    Adjust test formatting

--- spec/features/admin_can_login_spec.rb
+++ spec/features/admin_can_login_spec.rb
@@ -1,38 +1,35 @@
 require "rails_helper"
 
-RSpec.feature "Admin logs in" do
-  scenario "with correct info" do
-    expect(Admin.all.count).to eq 0
-    admin = Admin.create(email: "test@test.com", password: "correct")
-    expect(Admin.all.count).to eq 1
+feature "Admin logs in" do
+  let!(:admin) { Admin.create(email: "test@test.com", password: "correct") }
+  context "with correct info" do
+    it 'redirects to dashboard and displays confirmation' do
+      visit new_admin_session_path
 
-    visit new_admin_session_path
+      fill_in :admin_email, with: "test@test.com"
+      fill_in :admin_password, with: "correct"
 
-    fill_in :admin_email, with: "test@test.com"
-    fill_in :admin_password, with: "correct"
+      click_on "Log in"
 
-    click_on "Log in"
-
-    expect(current_path).to eq admin_root_path
-    expect(page).to have_content "Signed in successfully"
-    expect(page).to have_content "Logout"
-    expect(page).not_to have_content "Login"
-    expect(Admin.all.count).to eq 1
+      expect(current_path).to eq admin_root_path
+      expect(page).to have_content "Signed in successfully"
+      expect(page).to have_content "Logout"
+      expect(page).not_to have_content "Login"
+    end
   end
 
-  scenario "with incorrect info" do
-    expect(Admin.all.count).to eq 0
-    admin = Admin.create(email: "test@test.com", password: "testtest")
-    expect(Admin.all.count).to eq 1
+  context "with incorrect info" do
+    it 'stays on the page and displays warning' do
+      visit new_admin_session_path
 
-    visit new_admin_session_path
+      fill_in :admin_email, with: "test@test.com"
+      fill_in :admin_password, with: "wrong"
 
-    fill_in :admin_email, with: "test@test.com"
-    fill_in :admin_password, with: "wrong"
+      click_on "Log in"
 
-    click_on "Log in"
-
-    expect(current_path).to eq new_admin_session_path
-    expect(page).to have_content "Invalid Email or password"
+      expect(current_path).to eq new_admin_session_path
+      expect(page).to have_content "Invalid Email or password"
+      expect(page).not_to have_content "Logout"
+    end
   end
 end
