d6f40109f30222cfd5adf155af2710037a50d3a9
Jared Roth <jared.roth@gmail.com>
2016-12-15 13:08:25 -0800

    Set 'Vendor' as User.type default

--- db/migrate/20161214170947_add_type_to_user.rb
+++ db/migrate/20161214170947_add_type_to_user.rb
@@ -1,6 +1,6 @@
 class AddTypeToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :type, :string
+    add_column :users, :type, :string, default: 'Vendor'
     remove_column :users, :admin
   end
 end
