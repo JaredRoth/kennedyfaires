adc77f5558f304bebca6552f53a94627156f4c25
Mark Miranda <markmiranda@MacPro-102.hsd1.co.comcast.net>
2017-01-10 21:09:34 -0700

    closes #27

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
+  context "validations" do
+    it { should belong_to :faire }
+    it { should have_many :event_spaces }
+    it { should have_many :applications }
+    it { should have_many(:space_businesses).through(:event_spaces) }
+    it { should have_many(:applied_businesses).through(:applications) }
+    it { should validate_presence_of :date }
+    it { should validate_presence_of :faire }
+  end
 
+  context "methods" do
+    before do
+      user = create(:admin)
+      create(:faire_with_events, admin: user)
+      @event = Event.first
+    end
+
+    it "#title - happy path" do
+      expect(@event.title).to eq("Street Faire")
+    end
+
+    it "#title - sad path" do
+      @event.update(alternate_title: "something cool")
+      @event.reload
+      expect(@event.title).to eq("something cool")
+    end
+  end
 end
--- spec/models/faire_spec.rb
+++ spec/models/faire_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Faire, type: :model do
+  context "validations" do
+    it { should belong_to :city }
+    it { should belong_to :admin }
+    it { should have_many :events }
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :admin }
+  end
+
+  context "methods" do
+    it "#full_title" do
+      user = create(:admin)
+      faire = create(:faire, admin: user)
+
+      expect(faire.full_title).to include("Street Faire")
+    end
+  end
 
 end
